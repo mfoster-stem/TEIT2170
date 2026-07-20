e556e4fa70edd4786a8245f2dd4cc669b9c026a6
mfoster-stem <156629672+mfoster-stem@users.noreply.github.com>
2026-07-20 17:48:14 -0600

    Update videos.js

--- module-1/videos.js
+++ module-1/videos.js
@@ -12,7 +12,7 @@
      those only play inside Canvas.
    ============================================================ */
 window.MODULE_VIDEOS = {
-  "1.1": "",   // Common Documentation
+  "1.1": "https://videos-cloudfront.jwpsrv.com/6a5f5ca5_a4b65a552ac09812944b497b554b476227b9190d/content/conversions/eG4T4kba/videos/wqthO8rL-31879800.mp4",   // Common Documentation
   "1.2": "",   // Life Cycle Management
   "1.3": ""    // Change & Configuration Management
 };
