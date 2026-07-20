bb960b8a5f4b012924b13fb88b77a8b9b9d3a0a5
mfoster-stem <156629672+mfoster-stem@users.noreply.github.com>
2026-07-20 17:51:05 -0600

    Update videos.js

--- module-1/videos.js
+++ module-1/videos.js
@@ -13,6 +13,6 @@
    ============================================================ */
 window.MODULE_VIDEOS = {
   "1.1": "https://videos-cloudfront.jwpsrv.com/6a5f5ca5_a4b65a552ac09812944b497b554b476227b9190d/content/conversions/eG4T4kba/videos/wqthO8rL-31879800.mp4",   // Common Documentation
-  "1.2": "",   // Life Cycle Management
-  "1.3": ""    // Change & Configuration Management
+  "1.2": "https://videos-cloudfront.jwpsrv.com/6a5f6011_2790e9441260b5b1d74bae9ad95602aa0c61ebe1/content/conversions/eG4T4kba/videos/gBgmeqM7-31879800.mp4",   // Life Cycle Management
+  "1.3": "https://videos-cloudfront.jwpsrv.com/6a5f606f_580589434a0f4d4b35af12c102bc59532abf0a15/content/conversions/eG4T4kba/videos/8WDUxThv-31879800.mp4"    // Change & Configuration Management
 };
